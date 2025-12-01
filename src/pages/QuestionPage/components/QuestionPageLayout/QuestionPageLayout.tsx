3d44e52777dc36854ae3cb0fe3049005243c3c78
Zdorov Yurii <zdorovWork@gmail.com>
2025-12-01 10:15:28 +0200

    typecheck

--- src/pages/QuestionPage/components/QuestionPageLayout/QuestionPageLayout.tsx
+++ src/pages/QuestionPage/components/QuestionPageLayout/QuestionPageLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Card } from "../../../components";
+import { Card } from "../../../../components";
 import "./QuestionPageLayout.css";
 
 interface QuestionPageLayoutProps {
